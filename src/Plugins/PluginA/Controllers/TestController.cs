440cf3d9c46de33b872c554504934c80c4f7b49b
GodHei <37571429@qq.com>
2025-08-12 15:13:12 +0800

    日志调试完成

--- src/Plugins/PluginA/Controllers/TestController.cs
+++ src/Plugins/PluginA/Controllers/TestController.cs
@@ -1,7 +1,9 @@
+using Fastdotnet.Core.Exceptions;
+using Fastdotnet.Core.Utils;
 using Microsoft.AspNetCore.Mvc;
+using PluginA.Entities;
 using SqlSugar;
 using System.Threading.Tasks;
-using PluginA.Entities;
 
 namespace PluginA.Controllers
 {
@@ -19,6 +21,18 @@ namespace PluginA.Controllers
         [HttpGet]
         public async Task<IActionResult> Get()
         {
+            // 添加Debug日志测试
+            //DebugLogger.Print("开始执行DisablePlugin方法", "PluginA");
+            //_logger.LogInformation("正在尝试停用插件: {PluginId}", pluginId);
+
+            //// 添加可控异常测试
+            //throw new BusinessException("这是一个全局可控异常测试不会被记录到异常库-PluginA触发");
+
+            //// 添加不可控异常测试
+            //throw new Exception("这是一个全局不可控异常测试记录到异常库-PluginA触发");
+
+            // 添加Debug日志测试 - 方法结束
+            //DebugLogger.Print("DisablePlugin方法执行完成", "PluginA");
             var entities = await _db.Queryable<PluginEntity>().ToListAsync();
             return Ok(entities);
         }
