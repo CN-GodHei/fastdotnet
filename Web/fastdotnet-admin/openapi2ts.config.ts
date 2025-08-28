26dddc3dd945dd1786a375e5f35262b57a9f3f29
GodHei <37571429@qq.com>
2025-08-28 10:35:58 +0800

    项目切换为pnpm

--- Web/fastdotnet-admin/openapi2ts.config.ts
+++ Web/fastdotnet-admin/openapi2ts.config.ts
@@ -1,4 +1,10 @@
-export default {
-  schemaPath: 'http://petstore.swagger.io/v2/swagger.json',
-  serversPath: './servers',
+export default [
+    {
+  schemaPath: 'http://localhost:18889/swagger/main/swagger.json',
+  serversPath: './src/api',
+  projectName:'admin-base',//项目名称
+  namespace:"APIModel",//默认API
+  enumStyle:"enum",//枚举样式	string-literal | enum
+  requestImportStatement:"import request from '/@/utils/request'"
 }
+]
