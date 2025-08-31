9fa96c39770c41f35aaa890884e88a8e187cb17e
GodHei <37571429@qq.com>
2025-08-31 15:26:49 +0800

    插件商城后端代码结构增加

---
+++ src/Plugins/Plugin.Marketplace/Controllers/KeyGenerationController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Security.Cryptography;
+
+namespace Fastdotnet.Plugin.Marketplace.Controllers
+{
+    [ApiController]
+    [Route("api/marketplace/[controller]")]
+    public class KeyGenerationController : ControllerBase
+    {
+        /// <summary>
+        /// [仅限开发环境使用] 生成一个新的 RSA 密钥对 (4096 位)。
+        /// </summary>
+        /// <remarks>
+        /// **安全警告:** 请妥善保管生成的私钥，切勿泄露或提交到版本控制系统。
+        /// 公钥应硬编码到 Fastdotnet.Plugin.Core 中。
+        /// 私钥应使用 user-secrets 或其他安全机制存储。
+        /// </remarks>
+        /// <returns>包含公钥和私钥的密钥对。</returns>
+        [HttpGet("generate")]
+        [AllowAnonymous]
+        public IActionResult GenerateKeys()
+        {
+            try
+            {
+                using (var rsa = new RSACryptoServiceProvider(4096))
+                {
+                    // 导出公钥和私钥
+                    var publicKey = Convert.ToBase64String(rsa.ExportRSAPublicKey());
+                    var privateKey = Convert.ToBase64String(rsa.ExportRSAPrivateKey());
+
+                    return Ok(new
+                    {
+                        PublicKey = publicKey,
+                        PrivateKey = privateKey,
+                        Note = "Please store the PrivateKey securely and do not commit it to version control."
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, $"An error occurred while generating keys: {ex.Message}");
+            }
+        }
+    }
+}
---
+++ src/Plugins/Plugin.Marketplace/PluginMarketplace.cs
@@ -0,0 +1,44 @@
+using Autofac;
+using Fastdotnet.Plugin.Contracts;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Threading.Tasks;
+
+namespace Fastdotnet.Plugin.Marketplace
+{
+    public class PluginMarketplace : IPlugin
+    {
+        public string Name => "Plugin.Marketplace";
+        public string Version => "1.0.0";
+
+        public Task InitializeAsync(IServiceProvider serviceProvider)
+        {
+            // TODO: 在此执行插件初始化逻辑，例如加载配置
+            return Task.CompletedTask;
+        }
+
+        public Task StartAsync()
+        {
+            // TODO: 插件启动逻辑
+            return Task.CompletedTask;
+        }
+
+        public Task StopAsync()
+        {
+            // TODO: 插件停止逻辑
+            return Task.CompletedTask;
+        }
+
+        public Task UnloadAsync(IServiceProvider serviceProvider)
+        {
+            // TODO: 在此执行插件卸载和资源清理逻辑
+            return Task.CompletedTask;
+        }
+
+        public void ConfigureServices(ContainerBuilder builder)
+        {
+            // 在此注册插件内部的服务
+            // 例如: builder.RegisterType<MyMarketplaceService>().As<IMarketplaceService>().InstancePerLifetimeScope();
+        }
+    }
+}
