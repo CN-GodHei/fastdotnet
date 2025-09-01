02b760335de06b5070e89d2e04451cb2a564ebc8
GodHei <37571429@qq.com>
2025-09-01 11:00:21 +0800

    实体信息修改

--- src/Fastdotnet.Core/Models/Base/BaseEntity.cs
+++ src/Fastdotnet.Core/Models/Base/BaseEntity.cs
@@ -33,30 +33,31 @@ namespace Fastdotnet.Core.Models.Base
         /// <summary>
         /// 主键ID
         /// </summary>
-        [SugarColumn(IsPrimaryKey = true)]
+        [SugarColumn(ColumnName = "id", IsPrimaryKey = true, ColumnDescription="主键id")]
         public string Id { get; set; }
 
         /// <summary>
         /// 创建时间
         /// </summary>
-        [SugarColumn(ColumnDataType = "datetime")]
+        [SugarColumn(ColumnName = "create_time", ColumnDescription = "创建时间", ColumnDataType = "datetime")]
         public DateTime CreateTime { get; set; } = DateTime.Now;
 
         /// <summary>
         /// 更新时间
         /// </summary>
-        [SugarColumn(IsNullable = true, ColumnDataType = "datetime")]
+        [SugarColumn(ColumnName = "update_time", ColumnDescription = "更新时间", IsNullable = true, ColumnDataType = "datetime")]
         public DateTime? UpdateTime { get; set; }
 
         /// <summary>
         /// 是否删除
         /// </summary>
+        [SugarColumn(ColumnName = "is_deleted", ColumnDescription = "是否删除")]
         public bool IsDeleted { get; set; } = false;
 
         /// <summary>
         /// 删除时间
         /// </summary>
-        [SugarColumn(IsNullable = true, ColumnDataType = "datetime")]
+        [SugarColumn(ColumnName = "delete_time", ColumnDescription = "删除时间", IsNullable = true, ColumnDataType = "datetime")]
         public DateTime? DeleteTime { get; set; }
     }
 }
