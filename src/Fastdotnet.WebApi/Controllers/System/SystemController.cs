30705c8882787450ca33c7f25af3c56a2db5b62a
GodHei <37571429@qq.com>
2025-09-02 16:42:45 +0800

    增加生成机器码接口

---
+++ src/Fastdotnet.WebApi/Controllers/System/SystemController.cs
@@ -0,0 +1,32 @@
+﻿using Fastdotnet.Plugin.Core.Security;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Fastdotnet.Plugin.Core.Security;
+namespace Fastdotnet.WebApi.Controllers.System
+{
+    /// <summary>
+    /// 系统信息控制器
+    /// </summary>
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize] //
+    public class SystemController : ControllerBase
+    {
+        /// <summary>
+        /// 获取当前服务器的唯一机器指纹
+        /// </summary>
+        /// <returns>服务器的机器指纹字符串</returns>
+        [HttpGet("machine-fingerprint")]
+        [ProducesResponseType(typeof(string), 200)]
+        [AllowAnonymous]
+        public IActionResult GetMachineFingerprint()
+        {
+            var fingerprint = LicenseValidator.GenerateMachineFingerprint();
+            //if (string.IsNullOrEmpty(fingerprint))
+            //{
+            //    return Problem("Failed to generate machine fingerprint.");
+            //}
+            return Ok(fingerprint);
+        }
+    }
+}
