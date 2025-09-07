fcbde8d806aec471818da7dcc0b440a109b95cc7
GodHei <37571429@qq.com>
2025-09-07 19:05:00 +0800

    项目结构调整，删减代码

--- Web/test/pluginc/src/App.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <div style="background: lightcyan; border: 2px solid blue; padding: 20px;">
-    <h1>Hello from Plugin C!</h1>
-    <p>This is the minimal micro-app.</p>
-    <p>If you can see this, qiankun is working correctly.</p>
-  </div>
-</template>
