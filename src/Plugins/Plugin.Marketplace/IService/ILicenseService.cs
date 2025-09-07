65e5fbb14c1e3b3792dae4db451ac3d727922c95
GodHei <37571429@qq.com>
2025-09-07 20:08:42 +0800

    开源项目中移除非必要代码

--- src/Plugins/Plugin.Marketplace/IService/ILicenseService.cs
+++
@@ -1,9 +0,0 @@
-using Fastdotnet.Plugin.Marketplace.Dto;
-
-namespace Fastdotnet.Plugin.Marketplace.IService
-{
-    public interface ILicenseService
-    {
-        LicenseFileDto GenerateLicense(string pluginId, string userId, string licenseType, string machineFingerprint);
-    }
-}
