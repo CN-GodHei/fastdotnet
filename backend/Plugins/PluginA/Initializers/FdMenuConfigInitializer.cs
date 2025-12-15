363a3b05c51b73bc2ad0a8b74af30ba5181b4755
GodHei <37571429@qq.com>
2025-12-15 21:54:45 +0800

    插入菜单前检测是否已存在插件相关的菜单

--- backend/Plugins/PluginA/Initializers/FdMenuConfigInitializer.cs
+++ backend/Plugins/PluginA/Initializers/FdMenuConfigInitializer.cs
@@ -1,4 +1,4 @@
-﻿using Fastdotnet.Core.Entities.System;
+using Fastdotnet.Core.Entities.System;
 using Fastdotnet.Core.Enum;
 using Fastdotnet.Core.Initializers;
 using Fastdotnet.Core.IService;
@@ -26,13 +26,6 @@ namespace PluginA.Initializers
         {
             //_logger.LogInformation("Start: Initializing System Config...");
 
-            if (await _systemConfigRepository.ExistsAsync(a => a.Id != null))
-            {
-                //_logger.LogInformation("System config already seeded. Skipping initialization.");
-                return;
-            }
-
-
             var configs = new List<FdMenu>
             {
             new FdMenu {Title="Plugin A", Name = "", Code = "MENU_CODE_11375905679934469", Path = "/micro/11375910391972869", Icon = "ele-Monitor", Sort = 100, Type = 0, Module = "plugin-a", Category = "Admin", IsExternal = false, ExternalUrl = "", IsEnabled = true, PermissionCode = "", Component = "layout/routerView/parent.vue", IsHide = false, IsKeepAlive = true, IsAffix = false, IsIframe = false, IsFdMicroApp = false ,ParentCode=null},
@@ -40,7 +33,18 @@ namespace PluginA.Initializers
             new FdMenu {Title="Plugin A About", Name = "", Code = "MENU_CODE_11375913497723909", Path = "/micro/11375910391972869/about", Icon = "ele-InfoFilled", Sort = 2, Type = MenuType.Menu, Module = "plugin-a", Category = "Admin", IsExternal = false, ExternalUrl = "", IsEnabled = true, PermissionCode = "", Component = "", IsHide = false, IsKeepAlive = true, IsAffix = false, IsIframe = false, IsFdMicroApp = true ,ParentCode="MENU_CODE_11375905679934469",PluginId="11375910391972869"},
             new FdMenu {Title="SignalRDemo", Name = "", Code = "MENU_CODE_11375913497723910", Path = "/micro/11375910391972869/signalr-demo", Icon = "ele-InfoFilled", Sort = 2, Type = MenuType.Menu, Module = "plugin-a", Category = "Admin", IsExternal = false, ExternalUrl = "", IsEnabled = true, PermissionCode = "", Component = "", IsHide = false, IsKeepAlive = true, IsAffix = false, IsIframe = false, IsFdMicroApp = true ,ParentCode="MENU_CODE_11375905679934469",PluginId="11375910391972869"},
             };
-            await _systemConfigRepository.InsertRangeAsync(configs);
+            
+            // 直接使用条件查询已存在的菜单项
+            var existingMenus = await _systemConfigRepository.GetListAsync(m => configs.Select(c => c.Code).Contains(m.Code));
+            var existingCodes = existingMenus.Select(m => m.Code).ToHashSet();
+            
+            // 只插入不存在的菜单项
+            var menusToInsert = configs.Where(c => !existingCodes.Contains(c.Code)).ToList();
+            
+            if (menusToInsert.Any())
+            {
+                await _systemConfigRepository.InsertRangeAsync(menusToInsert);
+            }
 
             //_logger.LogInformation("Finish: System Config initialization complete.");
         }
