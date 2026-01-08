091ab7a775ba7e949a5551d5673dfbc446e495a9
GodHei <37571429@qq.com>
2026-01-09 00:04:34 +0800

    从通用方法中获取插件信息

--- backend/Plugins/PluginA/Initializers/PluginConfigurationInitializer.cs
+++ backend/Plugins/PluginA/Initializers/PluginConfigurationInitializer.cs
@@ -30,15 +30,15 @@ namespace PluginA.Initializers
         {
             // 动态获取当前插件的ID，而不是使用硬编码的ID
             var pluginInfo = PluginContext.GetCurrentPluginInfo();
-            var pluginId = pluginInfo?.id;
-            Console.WriteLine("获取到插件信息:" + pluginInfo.id);
+            //var pluginId = pluginInfo?.id;
+            //Console.WriteLine("获取到插件信息:" + pluginInfo.id);
             var defaultConfig = new MyPluginConfiguration()
             {
                 AliOssInfo = new AliOss() { AK = "1", SK = "2" },
                 AliPayInfo = new AliPay() { AppId = "商户Id", Secret = "密钥111" },
                 testinfo ="测试增加",
             };
-            var existingConfig = await _pluginConfigurationService.GetSettingsAsync<MyPluginConfiguration>("11375910391972869");
+            var existingConfig = await _pluginConfigurationService.GetSettingsAsync<MyPluginConfiguration>(pluginInfo.id);
 
             //Console.WriteLine("defaultConfig: " + JsonConvert.SerializeObject(defaultConfig));
             //Console.WriteLine("Existing: " + JsonConvert.SerializeObject(existingConfig));
@@ -46,7 +46,7 @@ namespace PluginA.Initializers
             var mergedConfig = ObjectMerger.ApplyOverrides(defaultConfig,existingConfig);
             //Console.WriteLine("mergedConfig: " + JsonConvert.SerializeObject(mergedConfig));
 
-            await _pluginConfigurationService.SaveSettingsAsync("11375910391972869", mergedConfig);
+            await _pluginConfigurationService.SaveSettingsAsync(pluginInfo.id, mergedConfig);
         }
     }
 }
