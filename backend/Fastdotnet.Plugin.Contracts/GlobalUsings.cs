8d7924930209e55850cfe8df2aa23c0897d99fcf
GodHei <37571429@qq.com>
2026-02-01 15:38:08 +0800

    各个类库抽取GlobalUsings

---
+++ backend/Fastdotnet.Plugin.Contracts/GlobalUsings.cs
@@ -0,0 +1,15 @@
+global using System;
+global using System.Collections.Concurrent;
+global using System.Collections.Generic;
+global using System.IO;
+global using System.Linq;
+global using System.Reflection;
+global using System.Text;
+global using System.Text.Json;
+global using System.Text.Json.Serialization;
+global using System.Threading.Tasks;
+global using Autofac;
+global using Microsoft.AspNetCore.Builder;
+global using Microsoft.AspNetCore.Routing;
+global using Microsoft.Extensions.DependencyInjection;
+global using System.Threading;
