6850748ea5e1eeb0d49ddd44bfcb6307b8f8cf39
GodHei <37571429@qq.com>
2026-02-02 16:45:57 +0800

    增加通用DTO

---
+++ backend/Fastdotnet.Core/Dtos/Common/CommonDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Fastdotnet.Core.Dtos.Common
+{
+    public record IdNameDto(string Id, string Name);
+    public record IdNameDto<TId>(TId Id, string Name);
+
+    public record IdNameStatusDto(string Id, string Name, DataStatus DataStatus);
+    public record IdNameStatusDto<TId>(TId Id, string Name, DataStatus DataStatus);
+}
