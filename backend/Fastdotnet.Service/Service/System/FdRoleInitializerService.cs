cfe3a35f3256c8377c23e1e27be7db7e73ce9e7d
GodHei <37571429@qq.com>
2026-02-09 16:08:17 +0800

    注释添加

--- backend/Fastdotnet.Service/Service/System/FdRoleInitializerService.cs
+++ backend/Fastdotnet.Service/Service/System/FdRoleInitializerService.cs
@@ -18,8 +18,8 @@ namespace Fastdotnet.Service.Service.System
             var entitys = new List<FdRole>
             {
                 new FdRole{Name = "超级管理",Code = "SUPER_ADMIN", IsSystem = true,Description = "拥有系统所有权限", Belong= SystemCategory.Admin},
-                new FdRole{Name = "普通管理",Code = "ADMIN_ROLE_001", IsSystem = true,Description = "普通管理",IsDefault=true, Belong= SystemCategory.Admin},
-                new FdRole{Name = "默认角色",Code = "APP_ROLE_001", IsSystem = true,Description = "APP端默认角色",IsDefault=true, Belong= SystemCategory.App},
+                new FdRole{Name = "普通管理",Code = "ADMIN_ROLE_001", IsSystem = true,Description = "默认管理(主要是管理能新加的管理员正常登录，具体的功能还是需要自己加角色)",IsDefault=true, Belong= SystemCategory.Admin},
+                new FdRole{Name = "默认角色",Code = "APP_ROLE_001", IsSystem = true,Description = "APP端默认角色(主要是使新加用户能正常登录，具体的功能还是需要自己加角色)",IsDefault=true, Belong= SystemCategory.App},
             };
 
             // 直接使用条件查询已存在的项
