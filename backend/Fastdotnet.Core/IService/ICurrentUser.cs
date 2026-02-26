9f37e2970169b73b49d79c5eb522d9e1077a1cbf
GodHei <37571429@qq.com>
2026-02-26 16:54:56 +0800

    超管判断加到当前用户服务中

--- backend/Fastdotnet.Core/IService/ICurrentUser.cs
+++ backend/Fastdotnet.Core/IService/ICurrentUser.cs
@@ -6,5 +6,6 @@ namespace Fastdotnet.Core.IService
         string? Id { get; }
         string UserName { get; }
         string UserType { get; } // 用于区分 Admin/App
+        bool IsSuperAdmin { get; } // 是否为超级管理员
     }
 }
--- backend/Fastdotnet.Core/Services/System/CurrentUser.cs
+++ backend/Fastdotnet.Core/Services/System/CurrentUser.cs
@@ -1,10 +1,14 @@
 
+using Fastdotnet.Core.Constants;
+using static Fastdotnet.Core.Constants.Permissions.Admin;
+
 namespace Fastdotnet.Core.Services.System
 {
     public class CurrentUser : ICurrentUser
     {
         private readonly IHttpContextAccessor _httpContextAccessor;
-
+        private readonly IRepository<FdAdminUserRole> _adminUserRoleRepository;
+        private readonly IRepository<FdRole> _roleRepository;
         public CurrentUser(IHttpContextAccessor httpContextAccessor)
         {
             _httpContextAccessor = httpContextAccessor;
@@ -36,15 +40,44 @@ namespace Fastdotnet.Core.Services.System
             {
                 // 首先尝试从token中获取category
                 var userType = User?.Claims.FirstOrDefault(c => c.Type == "category")?.Value;
-                
+
                 // 如果token中没有category，则从请求头中获取System-Belong
                 if (string.IsNullOrEmpty(userType))
                 {
                     userType = _httpContextAccessor.HttpContext?.Request.Headers["System-Belong"].FirstOrDefault();
                 }
-                
+
                 // 如果都没有获取到，返回默认值"Admin"
                 return string.IsNullOrEmpty(userType) ? "Admin" : userType;
+            }
+        }
+
+        /// <summary>
+        /// 只有管理端才用
+        /// </summary>
+        public bool IsSuperAdmin
+        {
+            get
+            {
+                var userId = User?.Claims.FirstOrDefault(c => c.Type == ClaimTypes.NameIdentifier)?.Value;
+                if (string.IsNullOrEmpty(userId))
+                {
+                    return false;
+                }
+                // 获取用户的角色
+                var userRoles = _adminUserRoleRepository.GetListAsync(ur => ur.AdminUserId == userId).Result;
+                var roleIds = userRoles.Select(ur => ur.RoleId).ToList();
+
+                if (!roleIds.Any())
+                {
+                    return false;
+                }
+
+                // 获取角色信息
+                var roles =  _roleRepository.GetListAsync(r => roleIds.Contains(r.Id)).Result;
+
+                // 检查是否包含超管角色
+                return roles.Any(r => r.Code == SystemConstants.SuperAdminRoleCode);
             }
         }
     }
