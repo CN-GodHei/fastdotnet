19d4ea30f3fcc2e73ac45e349a0403bdba5c836d
GodHei <37571429@qq.com>
2026-03-02 16:48:59 +0800

    增加系统配置字典

--- backend/Fastdotnet.Service/Initializers/FdDictTypeInitializer.cs
+++ backend/Fastdotnet.Service/Initializers/FdDictTypeInitializer.cs
@@ -33,6 +33,7 @@ namespace Fastdotnet.Service.Initializers
                 new FdDictType{ Id="11921994044146606", Name="代码生成打印类型", Code="CODE_06", SysFlag=YesNoEnum.Y, OrderNo=105, Remark="代码生成打印类型", Status=StatusEnum.Enable },
                 new FdDictType{ Id="11921994044146607", Name="机构类型", Code="CODE_07", SysFlag=YesNoEnum.Y, OrderNo=201, Remark="机构类型", Status=StatusEnum.Enable },
                 new FdDictType{ Id="11921994044146608", Name="邮箱操作业务名称", Code="CODE_08", SysFlag=YesNoEnum.Y, OrderNo=201, Remark="业务名称", Status=StatusEnum.Enable },
+                new FdDictType{ Id="11921994044146609", Name="系统配置", Code="CODE_09", SysFlag=YesNoEnum.Y, OrderNo=201, Remark="系统配置", Status=StatusEnum.Enable },
             };
             await _repository.InsertRangeAsync(fdDictTypeEntries);
         }
