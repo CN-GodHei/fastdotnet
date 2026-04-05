fcf6eac6f595f6e6f1fea9bac37d8c05f01bf0f8
GodHei <37571429@qq.com>
2026-04-05 13:12:21 +0800

    生产环境修改

--- Web/fastdotnet-admin/src/views/system/plugin/marketplace-iframe.vue
+++ Web/fastdotnet-admin/src/views/system/plugin/marketplace-iframe.vue
@@ -99,7 +99,7 @@ const canGoBack = ref(false)
 const canGoForward = ref(false)
 
 // iframe 源地址 - 指向我们新创建的插件管理页面
-const iframeSrc = ref('http://localhost:3000/plugin-manager/embedded?layout=none') // 指向 Nuxt 项目的插件管理页面
+const iframeSrc = ref('https://fastdotnet.top/plugin-manager/embedded?layout=none') // 指向 Nuxt 项目的插件管理页面
 
 // 记录开始加载时间
 const loadStartTime = ref<number>(0)
