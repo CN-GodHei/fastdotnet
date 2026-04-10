7ad030c6dcba0904907c1d4fb7658b5de6bfcfa9
GodHei <37571429@qq.com>
2026-04-11 02:30:26 +0800

    字段类型变更

--- backend/Fastdotnet.Core/Entities/Oidc/OpenIddictSqlSugarApplication.cs
+++ backend/Fastdotnet.Core/Entities/Oidc/OpenIddictSqlSugarApplication.cs
@@ -24,13 +24,13 @@ public class OpenIddictSqlSugarApplication
     /// <summary>
     /// 获取或设置当前应用程序的应用类型
     /// </summary>
-    [SugarColumn(Length = 50,IsNullable = true)]
+    [SugarColumn(Length = 50, IsNullable = true)]
     public virtual string? ApplicationType { get; set; }
 
     /// <summary>
     /// 获取或设置当前应用程序的客户端标识符
     /// </summary>
-    [SugarColumn(Length = 100,IsNullable = true)]
+    [SugarColumn(Length = 100, IsNullable = true)]
     public virtual string? ClientId { get; set; }
 
     /// <summary>
@@ -43,19 +43,19 @@ public class OpenIddictSqlSugarApplication
     /// <summary>
     /// 获取或设置当前应用程序的客户端类型
     /// </summary>
-    [SugarColumn(Length = 100,IsNullable = true)]
+    [SugarColumn(Length = 100, IsNullable = true)]
     public virtual string? ClientType { get; set; }
 
     /// <summary>
     /// 获取或设置并发令牌（用于乐观锁）
     /// </summary>
-    [SugarColumn(IsEnableUpdateVersionValidation = true,IsNullable = true)]
+    [SugarColumn(IsEnableUpdateVersionValidation = true, IsNullable = true)]
     public virtual Guid ConcurrencyToken { get; set; }
 
     /// <summary>
     /// 获取或设置当前应用程序的同意类型
     /// </summary>
-    [SugarColumn(Length = 50,IsNullable = true)]
+    [SugarColumn(Length = 50, IsNullable = true)]
     public virtual string? ConsentType { get; set; }
 
     /// <summary>
@@ -69,7 +69,7 @@ public class OpenIddictSqlSugarApplication
     /// </summary>
     [StringSyntax(StringSyntaxAttribute.Json)]
     [SugarColumn(IsNullable = true)]
-    public virtual string? DisplayNames { get; set; }    
+    public virtual string? DisplayNames { get; set; }
 
     /// <summary>
     /// 获取或设置与应用程序关联的 JSON Web Key Set，序列化为 JSON 对象
@@ -89,7 +89,7 @@ public class OpenIddictSqlSugarApplication
     /// 获取或设置与当前应用程序关联的登出后重定向 URI，序列化为 JSON 数组
     /// </summary>
     [StringSyntax(StringSyntaxAttribute.Json)]
-    [SugarColumn(IsNullable = true)]
+    [SugarColumn(IsNullable = true, ColumnDataType = "text")]
     public virtual string? PostLogoutRedirectUris { get; set; }
 
     /// <summary>
@@ -103,7 +103,7 @@ public class OpenIddictSqlSugarApplication
     /// 获取或设置与当前应用程序关联的重定向 URI，序列化为 JSON 数组
     /// </summary>
     [StringSyntax(StringSyntaxAttribute.Json)]
-    [SugarColumn(IsNullable = true)]
+    [SugarColumn(IsNullable = true, ColumnDataType = "text")]
     public virtual string? RedirectUris { get; set; }
 
     /// <summary>
