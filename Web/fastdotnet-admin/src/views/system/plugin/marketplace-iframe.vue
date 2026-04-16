8784f8a97e63798943dc066c7d74ce611a80d0f7
GodHei <37571429@qq.com>
2026-04-16 09:08:31 +0800

    链接修改

--- Web/fastdotnet-admin/src/views/system/plugin/marketplace-iframe.vue
+++ Web/fastdotnet-admin/src/views/system/plugin/marketplace-iframe.vue
@@ -151,8 +151,8 @@ const progressHistory = ref<any[]>([])
 const hasShownFailedMessage = ref(false)
 
 // iframe 源地址 - 指向我们新创建的插件管理页面
-// const iframeSrc = ref('https://fastdotnet.top/plugin-manager/embedded?layout=none') // 指向 Nuxt 项目的插件管理页面
-const iframeSrc = ref('http://localhost:3000/plugin-manager/embedded?layout=none') // 指向 Nuxt 项目的插件管理页面
+const iframeSrc = ref('https://fastdotnet.top/plugin-manager/embedded?layout=none') // 指向 Nuxt 项目的插件管理页面
+// const iframeSrc = ref('http://localhost:3000/plugin-manager/embedded?layout=none') // 指向 Nuxt 项目的插件管理页面
 
 // 记录开始加载时间
 const loadStartTime = ref<number>(0)
