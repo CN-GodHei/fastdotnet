01cb446e811fa77a1a69610c6c3f5516b0402a12
GodHei <37571429@qq.com>
2026-04-22 23:55:36 +0800

    增加utc保留特性

---
+++ backend/Fastdotnet.Core/Attributes/IsoDateTimeConverterUtc.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json.Converters;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace Fastdotnet.Core.Attributes
+{
+    // 1.一个专门处理 UTC 的 Converter
+    public class IsoDateTimeConverterUtc : IsoDateTimeConverter
+    {
+        public IsoDateTimeConverterUtc()
+        {
+            // 强制指定为 UTC
+            DateTimeStyles = System.Globalization.DateTimeStyles.AssumeUniversal;
+            // 格式化字符串，末尾加上 'Z' 表示 UTC
+            DateTimeFormat = "yyyy-MM-dd HH:mm:ssZ";
+        }
+    }
+}
