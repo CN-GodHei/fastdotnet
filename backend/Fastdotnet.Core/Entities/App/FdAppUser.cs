60b90f2628e33a54c8fc26406907895defeb1d4e
GodHei <37571429@qq.com>
2026-05-10 17:25:03 +0800

    移除用户邮箱唯一索引

--- backend/Fastdotnet.Core/Entities/App/FdAppUser.cs
+++ backend/Fastdotnet.Core/Entities/App/FdAppUser.cs
@@ -7,7 +7,7 @@ namespace Fastdotnet.Core.Entities.App
     /// </summary>
     [SugarTable("fd_app_user", "应用用户")]
     [SugarIndex("idx_app_username", nameof(Username), OrderByType.Asc, IsUnique = true)]
-    [SugarIndex("idx_app_email", nameof(Email), OrderByType.Asc, IsUnique = true)]
+    //[SugarIndex("idx_app_email", nameof(Email), OrderByType.Asc, IsUnique = true)]
     [SugarIndex("idx_app_phone", nameof(PhoneNumber), OrderByType.Asc, IsUnique = true)]
     public class FdAppUser : BaseEntity, ISoftDelete
     {
